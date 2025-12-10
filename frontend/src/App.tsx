c94f4bf4e777fefadbab6c12b3df05a3f19c9999
Tallal Mohar <tallalmohar@MacBookPro.lan>
2025-12-10 13:59:17 -0800

    added mobile button to hide sidebar when on mobile device

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,12 +4,37 @@ import Home from "./pages/Home";
 import Workouts from "./pages/Workouts";
 import Progress from "./pages/Progress";
 import Settings from "./pages/Settings";
+import { useState } from "react";
+import mobile_sidebar_btn from "../src/assets/mobile_sidebar_btn.png"
 
 function App() {
+	const [isOpen, setIsOpen] = useState(false);
   return (
-    <Router>
-      <div className="flex">
-        <SideBar />
+		<Router>
+		<div className="flex">
+		{/* Hamburger button - mobile only */}
+			<button 
+			className="md:hidden p-4 " 
+			onClick={() => setIsOpen(!isOpen)}
+			>
+			<img height={30} width={30} src={mobile_sidebar_btn} />
+			</button>
+
+		{isOpen && (
+  		<div className="md:hidden fixed inset-0 backdrop-blur-[3px] z-50">
+			<button 
+			onClick={() => setIsOpen(false)}
+			className="absolute top-4 right-4 text-2xl"
+			>
+			X
+			</button>
+			<SideBar />
+			</div>
+		)}
+		{/* Desktop sidebar - always visible */}
+		<div className="hidden md:block">
+		<SideBar />
+		</div>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/workouts" element={<Workouts />} />
